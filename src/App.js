c5d28d5218cddfedc3d1ce6d15680265bde96f97
Sorrab <ramonpbarros@gmail.com>
2020-08-29 23:14:42 -0700

    deploy my weather app to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+const api = {
+  key:"b0105367c10a309df73fedf0f8b8f403",
+  base:"https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  const [forecast, setForecast] = useState({});
+
+  const search = event => {
+    if(event.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result)
+        });
+
+      fetch(`${api.base}forecast?q=${query}&units=imperial&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setForecast(result);
+          console.log(result)
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  const convertDate = (dateVar) => {
+    dateVar = dateVar.replace(/(....).(..).(..)/, "$2/$3/$1");
+    return dateVar;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 60) ? 'app warm': 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text" 
+            className="search-bar" 
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.main.temp)}˚f
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : ('')}
+        {(typeof forecast.list != "undefined") ? (
+          <div>
+            <div className="forecast-box">
+              <div className="forecast">
+                <div className="">{convertDate((forecast.list[2].dt_txt).split(" ")[0])}</div>
+                <img src={`https://openweathermap.org/img/wn/${forecast.list[2].weather[0].icon}.png`} alt="" id="imgForecast0"></img>
+                <p>Temp: {Math.round(forecast.list[2].main.temp)}˚f</p>
+                <p>Humidity: {forecast.list[2].main.humidity}%</p>
+              </div>  
+              <div className="forecast">
+                <div className="">{convertDate((forecast.list[10].dt_txt).split(" ")[0])}</div>
+                <img src={`https://openweathermap.org/img/wn/${forecast.list[10].weather[0].icon}.png`} alt="" id="imgForecast0"></img>
+                <p>Temp: {Math.round(forecast.list[10].main.temp)}˚f</p>
+                <p>Humidity: {forecast.list[10].main.humidity}%</p>
+              </div>  
+              <div className="forecast">
+                <div className="">{convertDate((forecast.list[18].dt_txt).split(" ")[0])}</div>
+                <img src={`https://openweathermap.org/img/wn/${forecast.list[18].weather[0].icon}.png`} alt="" id="imgForecast0"></img>
+                <p>Temp: {Math.round(forecast.list[18].main.temp)}˚f</p>
+                <p>Humidity: {forecast.list[18].main.humidity}%</p>
+              </div>  
+              <div className="forecast">
+                <div className="">{convertDate((forecast.list[26].dt_txt).split(" ")[0])}</div>
+                <img src={`https://openweathermap.org/img/wn/${forecast.list[26].weather[0].icon}.png`} alt="" id="imgForecast0"></img>
+                <p>Temp: {Math.round(forecast.list[26].main.temp)}˚f</p>
+                <p>Humidity: {forecast.list[26].main.humidity}%</p>
+              </div>  
+              <div className="forecast">
+              <div className="">{convertDate((forecast.list[34].dt_txt).split(" ")[0])}</div>
+                <img src={`https://openweathermap.org/img/wn/${forecast.list[34].weather[0].icon}.png`} alt="" id="imgForecast0"></img>
+                <p>Temp: {Math.round(forecast.list[34].main.temp)}˚f</p>
+                <p>Humidity: {forecast.list[34].main.humidity}%</p>
+              </div>  
+            </div>
+          </div>
+        ) : ('')}
+      </main>
     </div>
   );
 }

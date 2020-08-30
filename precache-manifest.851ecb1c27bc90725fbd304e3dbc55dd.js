9ed9bc2252d33efe1dc5bc79adb9107bb170c160
Sorrab <ramonpbarros@gmail.com>
2020-08-29 23:15:37 -0700

    Updates

---
+++ precache-manifest.851ecb1c27bc90725fbd304e3dbc55dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a202f690f04a30b9c0083b5e40d9482a",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "c26351add381b3bbe1a3",
+    "url": "/react-weather/static/css/main.b4cf3c39.chunk.css"
+  },
+  {
+    "revision": "4a79708fe2bb8d98f2f2",
+    "url": "/react-weather/static/js/2.07144284.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather/static/js/2.07144284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c26351add381b3bbe1a3",
+    "url": "/react-weather/static/js/main.fc385eba.chunk.js"
+  },
+  {
+    "revision": "a48cdc162566cbfcf2df",
+    "url": "/react-weather/static/js/runtime-main.6ddbaee8.js"
+  },
+  {
+    "revision": "7f1d5fefff02e0199c3b00c9d6d80e2b",
+    "url": "/react-weather/static/media/cold.7f1d5fef.jpg"
+  },
+  {
+    "revision": "929f3850619c07445a7d233fa68e9a6c",
+    "url": "/react-weather/static/media/hot.929f3850.jpg"
+  }
+]);

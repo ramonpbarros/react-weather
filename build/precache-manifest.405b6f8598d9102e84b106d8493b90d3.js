c9a20f41aab6d0ec46d5dc211040f70017b2de65
Sorrab <ramonpbarros@gmail.com>
2020-09-02 12:31:25 -0700

    added gitignore

---
+++ build/precache-manifest.405b6f8598d9102e84b106d8493b90d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b53bc1f120c58a3f3d718d2a2178aa",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "9094cd1f71a35f07d6bb",
+    "url": "/react-weather/static/css/main.65c50d91.chunk.css"
+  },
+  {
+    "revision": "4a79708fe2bb8d98f2f2",
+    "url": "/react-weather/static/js/2.07144284.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather/static/js/2.07144284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9094cd1f71a35f07d6bb",
+    "url": "/react-weather/static/js/main.c879a4f7.chunk.js"
+  },
+  {
+    "revision": "a48cdc162566cbfcf2df",
+    "url": "/react-weather/static/js/runtime-main.6ddbaee8.js"
+  },
+  {
+    "revision": "929f3850619c07445a7d233fa68e9a6c",
+    "url": "/react-weather/static/media/hot.929f3850.jpg"
+  }
+]);
